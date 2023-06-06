c48cf61f358e3c832c56da1b6d0cc41e6b6752be
Tatiana Vetryachenko <vetryachenkotanya@gmail.com>
2023-06-06 10:19:11 +0300

    Create MainViewModel.cs

---
+++ Lab2_Vetriachenko/Lab2/ViewModels/MainViewModel.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Microsoft.Maui.Controls;
+using System.Net.Http;
+using System.Text.Json;
+using System.Numerics;
+
+namespace Lab2.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _currentDateTime;
+        private string _currentDeviceInfo;
+        private string _currentImageSource = "";
+        
+        public string CurrentDateTime
+        {
+            get => _currentDateTime;
+            set
+            {
+                _currentDateTime = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string ImageSource
+        {
+            get => _currentImageSource;
+            set
+            {
+                _currentImageSource = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        public ICommand UpdateTimeCommand { get; }
+        public ICommand LoadImageCommand { get; }
+
+        public string CurrentDeviceinfo
+        {
+            get => new StringBuilder()
+            .AppendLine($"Model: {DeviceInfo.Model}")
+            .AppendLine($"Manufacturer: {DeviceInfo.Manufacturer}")
+            .AppendLine($"Platform: {DeviceInfo.Platform}")
+            .AppendLine($"OS Version: {DeviceInfo.VersionString}").ToString();
+            set
+            {
+                _currentDeviceInfo = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            UpdateTimeCommand = new Command(UpdateTime);
+            LoadImageCommand = new Command(LoadImage);
+
+            CurrentDateTime = DateTime.Now.ToString("F");
+            FetchDataFromApiAsync();
+        }
+        
+        private void UpdateTime()
+        {
+            CurrentDateTime = DateTime.Now.ToString("F");
+        }
+
+        private void LoadImage()
+        {
+            ImageSource = "first.png";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private async void FetchDataFromApiAsync()
+        {
+            await DatabaseService.Init();
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/todos/1");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                var toDoItem = JsonSerializer.Deserialize<ToDoItem>(json);
+                await DatabaseService.SaveItemAsync(toDoItem);
+            }
+        }
+
+        public double Add(int n, int b) => n + b;
+    }
+}
